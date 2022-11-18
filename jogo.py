48373c5c5d9d6d1bd2799d71940f30d65fc6d327
zorzan-ana <zorzan425@gmail.com>
2022-11-18 13:44:57 -0300

    criação da tela - abrir/fechar

---
+++ jogo.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.init()
+dis=pygame.display.set_mode((400,300))
+pygame.display.update()
+pygame.display.set_caption('Jogo da cobrinha!')
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            game_over=True
+ 
+pygame.quit()
+quit()
